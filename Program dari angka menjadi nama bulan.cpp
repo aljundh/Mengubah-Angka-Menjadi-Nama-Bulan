38f4a1894e3a382fa180fb912a9986114b8a82bc
Hafis Royhan <107126204+aljundh@users.noreply.github.com>
2022-11-26 22:56:29 +0700

    Add files via upload

---
+++ Program dari angka menjadi nama bulan.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main(){
+cout<<"======KALENDER======="<<endl;
+
+
+int a;
+
+cout<<"Masukkan Bulan Pilihan;";
+cin>>a;
+
+
+if (a==1){
+	cout<<"Bulan Januari ";
+	}else if(a==2){
+			cout<<" Bulan Februari";
+	}else if(a==3){
+			cout<<"Bulan Maret";
+	}else if(a==4){
+			cout<<" Bulan april";
+	}else if(a==5){
+			cout<<"Bula Mei";
+	}else if(a==6){
+			cout<<" Bulan Juni";
+	}else if(a==7){	
+			cout<<"Bulan juli";
+	}else if(a==8){	
+			cout<<"Bulan agustus";
+	}else if(a==9){	
+			cout<<" Bulan september";
+	}else if(a==10){	
+			cout<<"Bula Oktober";
+	}else if(a==11){	
+			cout<<" Bulan November";
+	}else if(a==12){	
+			cout<<" Bulan   Desember";
+	}else{
+		cout<<"error";
+	}
+	
+return 0;
+}
+
+
+
